b397df37b64074ce346db0e546296497f4d9a8af
Haku1993 <frjrodriguezla@unal.edu.co>
2019-06-05 20:41:06 -0500

    3 modificacion

--- DensidadProbabilidadR.py
+++ DensidadProbabilidadR.py
@@ -2,9 +2,8 @@ import scipy as sc
 import math  as mt
 import sympy as sp
 
-
-n=3
-l=2
+n=3 # numero cuantico n
+l=2 # numero cuantico l
 
 r=sp.symbols('r')
 x=sp.symbols('x')
@@ -15,13 +14,11 @@ j=2*n*(n+l)
 m=n-l-1
 k=2*l+1
 
-
 def L(m): #polinomio de Laguerre
   f=sp.exp(-x)*pow(x,m+k) 
   ff=sp.diff(f,x,m) #Derivada m-sima
   L=sp.simplify(sp.exp(x)*pow(x,-k)/mt.factorial(m)*ff) #simplifica la expresion   
   return L.subs(x,a*r)
-
 
 def F(m,a):
   F=pow(a**3*mt.factorial(m)/mt.factorial(j),1/2)*sp.exp(-a*r/2)*pow(a,l)*pow(r,l+1)
@@ -37,6 +34,7 @@ err2p=0.43*10**(7)
 err3s=0.15*10**(15)
 err3p=0.04*10**(23)
 err3d=0.03*10**(31)
+
 for i in range(500):
     r=0.04*i
     print(r," ",err3d*mt.fabs(Ur(r))**2)
